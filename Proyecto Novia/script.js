7c5c50efbf355e770a79c505c39973bcb0d018a4
AdriRodri13 <adridev88@gmail.com>
2024-07-07 21:06:18 +0200

    Actualizacion

--- Proyecto Novia/script.js
+++ Proyecto Novia/script.js
@@ -12,6 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
     sonidos[2] = document.getElementById("misAmigasSonido");
     botones[3] = document.getElementById("mejorNovia");
     sonidos[3] = document.getElementById("mejorNoviaSonido");
+    botones[4] = document.getElementById("decisiones");
+    sonidos[4] = document.getElementById("decisionesSonido");
+    botones[5] = document.getElementById("aprendo");
+    sonidos[5] = document.getElementById("aprendoSonido");
+    botones[6] = document.getElementById("estupideces");
+    sonidos[6] = document.getElementById("estupidecesSonido");
+    botones[7] = document.getElementById("escuchar");
+    sonidos[7] = document.getElementById("escucharSonido");
 
     botones[0].addEventListener('click', () => {
         sonidos[0].play();
@@ -24,6 +32,18 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     botones[3].addEventListener('click', () => {
         sonidos[3].play();
+    })
+    botones[4].addEventListener('click', () => {
+        sonidos[4].play();
+    })
+    botones[5].addEventListener('click', () => {
+        sonidos[5].play();
+    })
+    botones[6].addEventListener('click', () => {
+        sonidos[6].play();
+    })
+    botones[7].addEventListener('click', () => {
+        sonidos[7].play();
     })
 })
 
